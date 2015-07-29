bcbfe3c8c44be36c8399a54c02a391f168fd2192
yuffie <yuffie.panda@gmail.com>
2015-07-29 21:13:41 +0800

    完善测试基类，使用spring版本换为4.1.7

--- src/test/java/com/yuffie/util/test/TestBase.java
+++ src/test/java/com/yuffie/util/test/TestBase.java
@@ -1,5 +1,7 @@
 package com.yuffie.util.test;
 
+import junit.framework.TestCase;
+
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -10,6 +12,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/spring/applicationContext.xml" })
-public class TestBase {
+public class TestBase extends TestCase{
 
 }
