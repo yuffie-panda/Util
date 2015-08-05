45aa0014074271c592d4e4b188af7a597021de39
yuffie <yuffie.panda@gmail.com>
2015-08-05 20:35:11 +0800

    添加多线程模板

---
+++ src/main/java/com/yuffie/util/thread/ThreadTemplate.java
@@ -0,0 +1,22 @@
+package com.yuffie.util.thread;
+
+/**
+ * 多线程-线程模板
+ * 
+ * @author Yuffie
+ *
+ */
+public class ThreadTemplate extends Thread {
+    
+    private Object args;
+
+    public ThreadTemplate(Object args) {
+
+        this.args = args;
+
+    }
+    
+    public void run() {
+        // TODO add function here
+    }
+}
--- src/test/java/com/yuffie/util/http/service/SimpleHttpRequestTest.java
+++ src/test/java/com/yuffie/util/http/service/SimpleHttpRequestTest.java
@@ -1,5 +1,6 @@
 package com.yuffie.util.http.service;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,11 +16,16 @@ public class SimpleHttpRequestTest extends TestBase {
 
     @Before
     public void setUp() {
-
+        // TODO add something that do before every test
     }
     
     @Test
     public void test() {
         // TODO add test case
+    }
+
+    @After
+    public void cleanSet() {
+        // TODO add something that do after every test
     }
 }
